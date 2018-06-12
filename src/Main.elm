f94987b8f14c57db4e8fadcd4e0e9f49005c4d1e
zooeymiller <zooeyxmiller@gmail.com>
2018-06-12 14:13:57 +0100

    adding initial grid functions

--- src/Main.elm
+++ src/Main.elm
@@ -2,18 +2,23 @@ module Main exposing (..)
 
 import Html exposing (Html, text, div, h1, img)
 import Html.Attributes exposing (src)
+import Array exposing (..)
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    {
+        game: Grid,
+        running: Bool
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( Model (createInitialGrid 100) False, Cmd.none )
 
 
 
@@ -53,3 +58,33 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+type Cell = Alive | Dead
+
+type alias Grid = Array (Array Cell)
+
+type alias Coordinate = (Int, Int)
+
+
+createInitialGrid : Int -> Grid
+createInitialGrid h = Array.initialize h <| always (Array.initialize h (always Alive))
+
+getInGrid : Grid -> Coordinate -> Maybe Cell
+getInGrid grid (x, y) = Maybe.andThen (Array.get y) <| Array.get x grid
+
+setInGrid : Coordinate -> Grid -> Cell -> Maybe Grid
+setInGrid (x, y) grid value = Array.get y grid |> Maybe.map (Array.set x value) |> Maybe.map (\v -> Array.set y v grid)
+
+findNeigbours x y grid = List.map (getInGrid grid) <| getNeigbourCoordinates x y
+
+getNeigbourCoordinates : Int -> Int -> List Coordinate
+getNeigbourCoordinates x y = [
+    (x - 1, y - 1)
+    , (x, y - 1)
+    , (x + 1, y - 1)
+    , (x - 1, y)
+    , (x + 1, y)
+    , (x - 1, y + 1)
+    , (x, y + 1)
+    , (x + 1, y + 1)
+    ]
